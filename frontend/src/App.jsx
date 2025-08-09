dba668eefb66f90092c97368e9f13b3b01b98fa2
anvitha <anvithaashok18@gmail.com>
2025-08-09 19:01:49 +0530

    Initial frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 const App = () => {
   return (
-    <div class="text-green-400 text-4xl">App</div>
-  )
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/sign-up" exact element={<SignUp />} />
+    </Routes>
+  </BrowserRouter>
+</>
+)
 }
 
 export default App
